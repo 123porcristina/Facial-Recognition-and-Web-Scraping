aada3226cef584fef012e562b59417bd99f23bd4
greggberneaquinolegarda <gregglegarda@gwu.edu>
2019-07-22 20:20:15 -0400

    update

--- Code/Main.py
+++ Code/Main.py
@@ -5,8 +5,8 @@ import sys
 
 # Get user supplied values
 imagePath = "abba.png"
-#imagePath = "messi5.jpg"
-imagePath = "one.jpg"
+#imagePath = "code/messi5.jpg"
+#imagePath = "code/one.jpg"
 cascPath = "haarcascade_frontalface_default.xml"
 
 # Create the haar cascade
@@ -16,7 +16,7 @@ faceCascade = cv2.CascadeClassifier(cascPath)
 image = cv2.imread(imagePath,0)
 #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-
+'''
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
                                      image,
@@ -31,7 +31,7 @@ print("Found {0} faces!".format(len(faces)))
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
+'''
 
 cv2.imshow("Faces found", image)
 cv2.waitKey(0)
