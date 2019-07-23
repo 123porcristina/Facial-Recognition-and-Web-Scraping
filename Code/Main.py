bddbfb3954515e1c2a776a71a4558628c83d85b3
greggberneaquinolegarda <gregglegarda@gwu.edu>
2019-07-22 20:41:01 -0400

    update

--- Code/Main.py
+++ Code/Main.py
@@ -5,8 +5,8 @@ import sys
 
 # Get user supplied values
 imagePath = "abba.png"
-#imagePath = "code/messi5.jpg"
-#imagePath = "code/one.jpg"
+#imagePath = "messi5.jpg"
+#imagePath = "one.jpg"
 cascPath = "Code/haarcascade_frontalface_default.xml"
 
 # Create the haar cascade
@@ -23,7 +23,7 @@ faces = faceCascade.detectMultiScale(
                                      scaleFactor=1.1,
                                      minNeighbors=5,
                                      minSize=(30, 30),
-                                     flags = cv2.CASCADE_SCALE_IMAGE
+                                     flags = cv2.cv.CV_HAAR_SCALE_IMAGE
                                      )
 
 print("Found {0} faces!".format(len(faces)))
